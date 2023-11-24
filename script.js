89c9074ff851fc39a12891802cffa140945d29ba
Nikolay <areyfclm@gmail.com>
2023-11-24 15:17:45 +0300

    mobile menu fixes

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const burger = document.querySelector('.burger')
 const mobileMenu = document.querySelector('.mobile-nav')
+const menuLinks = document.querySelectorAll('#menu li')
 
 burger.addEventListener('click', () => {
     mobileMenu.classList.toggle('visible')
 })
+
+menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        console.log('Works!');
+        mobileMenu.classList.remove('visible')
+    });
+  });
